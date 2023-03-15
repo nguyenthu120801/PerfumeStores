806c17d5c46bfb14421636a9210c37bb87d139bb
thinhhiep <75740575+thinhhiep@users.noreply.github.com>
2023-03-15 21:22:07 +0700

    .

--- PerfumeStores/PerfumeStores/Core/Models/Prn221Context.cs
+++ PerfumeStores/PerfumeStores/Core/Models/Prn221Context.cs
@@ -28,8 +28,15 @@ public partial class Prn221Context : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=localhost;database=PRN221;integrated security=true;TrustServerCertificate=True;");
+    {
+        var connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+        string connect = connectionString.GetConnectionString("CnnStr");
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(connect);
+        }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
